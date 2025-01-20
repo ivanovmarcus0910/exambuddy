6bb99f97d344ad17df12405e71fbf65e641fb9cf
Anh Thợ Code <113629275+ivanovmarcus0910@users.noreply.github.com>
2025-01-20 10:36:31 +0700

    Lần đầu

---
+++ src/main/java/com/example/exambuddy/ExamBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.exambuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exambuddy/ExamBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exambuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
